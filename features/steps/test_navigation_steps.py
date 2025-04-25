b9aa42e3fb06bf9495febfb3e621b6389f241e04
Kiran <84580493+kiransrik@users.noreply.github.com>
2025-04-25 15:22:56 +0100

    Update test_navigation_steps.py

--- features/steps/test_navigation_steps.py
+++ features/steps/test_navigation_steps.py
@@ -1,21 +1,23 @@
 from behave import given, when, then
-from playwright.sync_api import sync_playwright, expect
+from playwright.async_api import async_playwright
 from pages.navigation_page import NavigationPage
-import time
 
+# The 'given' step to start the browser
 @given("the user is on the homepage")
-def step_impl(context):
-    playwright = sync_playwright().start()
-    context.browser = playwright.chromium.launch(headless=False)
-    context.page = context.browser.new_page()
-    context.navigation = NavigationPage(context.page)
-    context.page.goto("https:///demo.guru99.com/popup.php")
+async def step_impl(context):
+    async with async_playwright() as playwright:
+        context.browser = await playwright.chromium.launch(headless=False)
+        context.page = await context.browser.new_page()
+        context.navigation = NavigationPage(context.page)
+        await context.page.goto("https://www.guru99.com")
 
+# The 'when' step for clicking the dropdown
 @when("the user clicks the Selenium dropdown")
-def step_impl(context):
-    context.navigation.open_selenium_dropdown()
+async def step_impl(context):
+    await context.navigation.open_selenium_dropdown()
 
-@then("the user should be able to visit each About subpage and see the correct title")
-def step_impl(context):
-    context.navigation.navigate_selenium_dropdown()
-    context.browser.close()
+# The 'then' step for navigating and validating the dropdown items
+@then("the user should be able to visit each Selenium subpage and see the correct title")
+async def step_impl(context):
+    await context.navigation.navigate_selenium_dropdown()
+    await context.browser.close()
